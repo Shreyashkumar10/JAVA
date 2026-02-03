e6f336ea2a428724f7ea3144ce63d709de86eef5
Shreyashkumar10 <200588966+Shreyashkumar10@users.noreply.github.com>
2026-02-03 23:22:09 +0530

    Add files via upload

---
+++ Java/Array.java
@@ -0,0 +1,16 @@
+//import java.util.*;
+
+public class Array {
+    public static void main  ( String arg[]  ){
+
+        int[] marks = new int[4];
+        marks[0] = 96;
+        marks[1] = 92;
+        marks[2] = 76;
+        marks[3] = 89;
+
+        for ( int i=0;i<6;i++) {
+            System.out.println( " " + marks[i]);
+        }
+    }    
+}
