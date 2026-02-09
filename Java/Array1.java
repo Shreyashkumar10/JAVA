2439f09535ca062b1117550b223235169733b4fa
Shreyashkumar10 <200588966+Shreyashkumar10@users.noreply.github.com>
2026-02-09 22:28:27 +0530

    Add files via upload

---
+++ Java/Array1.java
@@ -0,0 +1,12 @@
+
+public class Array1{
+    public static void main( String[]  arg ){
+
+        int arr[]={1,2,3,4,5,6} ;
+        
+        for (  int i =0 ;i<arr.length;i++ ){
+            System.out.println( arr[i]);
+        }
+
+    }
+}
