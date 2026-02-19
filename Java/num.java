70355b23799d8743362e6dd57309c66adae68624
Shreyashkumar10 <200588966+Shreyashkumar10@users.noreply.github.com>
2026-02-19 16:51:48 +0530

    Add files via upload

---
+++ Java/num.java
@@ -0,0 +1,22 @@
+public class num{
+      public static void main(String arg[] ){
+ 
+            System.out.println(" Hello World  ");
+           int a = 10;
+           int b = 20;
+            int sum = a + b;
+            String  g =  "hello world";
+            int diff = b-a;
+            int squ1= a*a;
+            int squ2 =b*b;
+
+            System.out.println("The first interger "+a);
+              System.out.println("The second integer "+b);  
+              System.out.println(" the sum is  " +sum);
+                System.out.println("The difference is " + diff);
+                System.out.println(g);
+                System.out.println(" The square root of  "+squ1);
+                  System.out.println(" The square root of  "+squ2);
+                        
+      }
+}
